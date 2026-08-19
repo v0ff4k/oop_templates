c17208526e68f09dac891de47f6234ef377ce2a0
worker <10612098+v0ff4k@users.noreply.github.com>
2026-08-19 13:27:04 +0600

    fix: update Creational/Singleton examples with DB and config single class creation

--- Creational/Singleton/DB.php
+++ Creational/Singleton/DB.php
@@ -1,10 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * Created by pSom.
  * User: 9r00+
  * at: 10.09.19 - 10:09
+ * upd: 5.01.24 - 12:27
+ * upd: 18.08.26 - 22:59
  */
+
+namespace Creational\Singleton;
+
+use \PDO;
 
 trait DB
 {
@@ -23,26 +31,37 @@ trait DB
     /** @var string $pass */
     private $pass;
 
-    private final function __clone() {}
+    /** @var PDO $pdo */
+    private $pdo;
 
-    private final function __wakeup() {}
-
-    private final function __construct()
+    final private function __construct()
     {
+        $this->loadConfig();
 
         $options = [
             PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"
         ];
 
-        $this->instance = new PDO(
-            'mysql:host='.$this->host.';dbname='.$this->name,
+        $this->pdo = new PDO(
+            'mysql:host=' . $this->host . ';dbname=' . $this->name,
             $this->user,
             $this->pass,
             $options
         );
     }
 
-    public static function connect($host, $name, $user, $pass)
+    /**
+     * COnfig init, get from .env/file/others
+     */
+    private function loadConfig(): void
+    {
+        $this->host = 'hostname';
+        $this->name = 'databaseName';
+        $this->user = 'username';
+        $this->pass = 'password';
+    }
+
+    public static function connect()
     {
         if (self::$conn != null) {
             return self::$conn;
@@ -50,12 +69,39 @@ trait DB
 
         return new self;
     }
+
+    // disable copy on clone
+
+    /**
+     * Get getPdo
+     * @return \PDO
+     */
+    public function getPdo(): \PDO
+    {
+        return $this->pdo;
+    }
+
+    final private function __clone()
+    {
+    }
+
+    // disable on deserialization(json->obj)
+    final private function __wakeup()
+    {
+    }
 }
 
-//client class with calling:::
+
+/**
+ * Клиентский код
+ */
 class PDOConnection
 {
     use DB;
 }
 
-$pdo = PDOConnection::connect('127.0.0.1', 'data_name', 'root', 'pass');
+$conn = PDOConnection::connect();
+$stmt = $conn->getPdo()->prepare('SELECT COUNT(*) as count FROM users');
+$stmt->execute([]);
+/** @var array $result */
+$result = $stmt->fetchAll();
--- Creational/Singleton/Preferences.php
+++ Creational/Singleton/Preferences.php
@@ -1,26 +1,41 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * Created by pSom.
  * User: 9r00+
  * at: 10.09.19 - 10:09
+ * upd: 5.01.24 - 12:28
+ * upd: 18.08.26 - 23:24
  */
+
+namespace Creational\Singleton;
 
 // for disable   new ReflectionClass("") replace "class" with "trait" !
 class Preferences
 {
     /** @var self */
     private static $instance;
-    private $properties = array();
+    private $properties = [];
 
     //forbid to create "new" instance  with finalize
-    private final function __construct() {}
-    //forbid clone for create another instance  with finalize
-    private final function __clone() {}
-    //forbid *serialize() for create another instance  with finalize
-    private final function __wakeup() {}
+    final private function __construct()
+    {
+    }
 
-    public static function getInstance() {
+    //forbid clone for create another instance  with finalize
+    final private function __clone()
+    {
+    }
+
+    //forbid *serialize() for create another instance  with finalize
+    final private function __wakeup()
+    {
+    }
+
+    public static function getInstance()
+    {
         if ((empty(static::$instance))) {
             static::$instance = new Preferences();
         }
@@ -28,21 +43,27 @@ class Preferences
         return static::$instance;
     }
 
-    public function getProperty($key) {
+    public function getProperty($key)
+    {
         return $this->properties[$key];
     }
 
-    public function setProperty($key, $value) {
+    public function setProperty($key, $value)
+    {
         $this->properties[$key] = $value;
     }
 }
 
-//client code:::
+
+/**
+ * Клиентский код
+ */
 $dataPref = Preferences::getInstance();
-$dataPref->setProperty('database', "mySQL");
+$dataPref->setProperty('database', 'mySQL');
 
 unset($dataPref);
 
 $anotherPref = Preferences::getInstance();
 $value =  $anotherPref->getProperty('database');
 print $value . " connected \n";// mySQL connected
+unset($anotherPref);
